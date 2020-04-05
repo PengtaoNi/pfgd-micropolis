cc8b0f960a8536745c206edbe6cb2e05dc75f46e
PengtaoNi <33943112+PengtaoNi@users.noreply.github.com>
2020-04-05 12:38:54 +0800

    phase 0 remove building
    phase 0: if click on a building, change the tiles of that building to dirt

--- src/micropolisj/engine/MoveTool.java
+++ src/micropolisj/engine/MoveTool.java
@@ -21,11 +21,49 @@ class MoveTool extends ToolStroke
 	boolean applyMove(ToolEffectIfc eff)
 	{
 		if (this.phase == 0) {
-			eff.setTile(0, 0, DIRT);
+			if (isZoneAny(eff.getTile(0, 0))) {
+				removeZone(eff);
+			}
 		}
 		else {
 			eff.setTile(0, 0, RUBBLE);
 		}
 		return true;
+	}
+	
+	void removeZone(ToolEffectIfc eff)
+	{
+		int currTile = eff.getTile(0, 0);
+		int dx = 0;
+		int dy = 0;
+		
+		for (dx = -3; dx <= 1; dx++) {
+			for (dy = -3; dy <= 1; dy++) {
+				if (isZoneCenter(eff.getTile(dx, dy))) {
+					currTile = eff.getTile(dx, dy);
+					break;
+				}
+			}
+			if (isZoneCenter(eff.getTile(dx, dy))) {
+				break;
+			}
+		}
+
+		// zone center bit is set
+		assert isZoneCenter(currTile);
+
+		CityDimension dim = getZoneSizeFor(currTile);
+		assert dim != null;
+		assert dim.width >= 3;
+		assert dim.height >= 3;
+
+		eff.spend(1);
+
+		for (int ddx = -1; ddx < dim.width; ddx++) {
+			for (int ddy = -1; ddy < dim.height; ddy++) {
+				eff.setTile(dx+ddx, dy+ddy, DIRT);
+			}
+		}
+		return;
 	}
 }
